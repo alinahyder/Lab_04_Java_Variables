521a41983f109490c0947c6407661724b28bed90
alinahyder <alina@granitewood.com>
2024-06-03 23:28:29 -0400

    init commit

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+
+public class Main {
+    public static void main(String[] args) {
+        double lunchCost = 10.95;
+        int kidsFamily = 5;
+        String rain = "yes";
+        double gasPrice = 2.50;
+        int favNumber= 9;
+        int shoeSize= 7;
+        int birthMonth= 4;
+        String fullName= "Alina Hyder";
+
+        System.out.println("The cost of my lunch is: " +lunchCost);
+        System.out.println("The number of kids in my family is " +  kidsFamily);
+        System.out.println("Is it raining? "+ rain);
+        System.out.println("The price of a gallon of gas is "+gasPrice);
+        System.out.println("My favorite number is "+ favNumber);
+        System.out.println("My shoe size is "+shoeSize);
+        System.out.println("My birth month is "+birthMonth);
+        System.out.println("My full name is "+fullName);
+
+
+
+    }
+}
